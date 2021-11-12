069929dde1e6f68678fe5d7956374e9d52d3a42b
FAXec <xecfahim.ahmed@gmail.com>
2021-11-12 00:17:40 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const alertBanner = document.getElementById("alert");
 
+
+
 let markup = `<div class ="alert-banner"> <p><strong>Alert:</strong>You have <strong>6</strong> overdue tasks to complete.</p>
 <p class="alert-banner-close">x</p>
 </div>`;
 
 alertBanner.innerHTML = markup;
+
+
+
+

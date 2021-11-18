136e1b2afff7a5cf245b9d728fec42af45da277d
FAXec <xecfahim.ahmed@gmail.com>
2021-11-18 03:39:23 +0000

    updated css

--- js/app.js
+++ js/app.js
@@ -7,9 +7,8 @@ const message = document.getElementById("messageField");
 const sendButton = document.getElementById("send");
 
 
-let markup = `<div class ="alert-banner"> <p><strong>Alert:</strong>You have <strong>6</strong> overdue tasks to complete.</p>
-<p class="alert-banner-close">x</p>
-</div>`;
+let markup = `<div class ="alert-banner"> <p><strong>Alert:</strong> You have unread messages.</p>
+<p class="alert-banner-close">x</p></div>`;
 
 alertBanner.innerHTML = markup;
 

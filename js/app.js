92483245c7c3445d8e863b0a3a8a911f61368e92
FAXec <xecfahim.ahmed@gmail.com>
2021-11-26 23:43:07 +0000

    updated css and js

--- js/app.js
+++ js/app.js
@@ -145,3 +145,18 @@ sendButton.addEventListener("click", function(e) {
 
 
 
+const close = document.getElementsByClassName("closebtn");
+let i;
+
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function(){
+    var div = this.parentElement;
+    div.style.opacity = "0";
+    setTimeout(function(){ div.style.display = "none"; }, 600);
+  }
+}
+
+
+
+
+
